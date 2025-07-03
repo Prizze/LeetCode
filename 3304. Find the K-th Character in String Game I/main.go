929cb80e3e6607ca4ec37dc6c2b297480016693e
Maksim shabalin <sabalin291@gmail.com>
2025-07-03 17:39:58 +0300

    Закоментил

--- 3304. Find the K-th Character in String Game I/main.go
+++ 3304. Find the K-th Character in String Game I/main.go
@@ -1,16 +1,16 @@
 package main
 
 func kthCharacter(k int) byte {
-	word := []byte{'a'}
-	for len(word) < k {
-		generatedWord := make([]byte, len(word))
-		var generatedChar byte
+	word := []byte{'a'} // Изначальное слово
+	for len(word) < k {	
+		generatedWord := make([]byte, len(word)) // Резервируем место той же длины
+		var generatedChar byte				     // Для посимвольной генерации
 
 		for i, char := range word {
 			switch char {
+			// Переход z -> a
 			case 'z':
 				generatedChar = 'a';
-				break;
 			default:
 				generatedChar = char + 1;
 			}
